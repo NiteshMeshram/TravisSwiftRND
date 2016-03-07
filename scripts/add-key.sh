4ba87e43946b26f6931186114dbe0b0f7f57ac18
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2016-03-07 18:20:19 +0530

    Modified Travis.yml

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -19,6 +19,12 @@ security unlock-keychain -p travis ios-build.keychain
 # see http://www.egeek.me/2013/02/23/jenkins-and-xcode-user-interaction-is-not-allowed/
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
+echo “APP Path - 1”
+echo $APP_PROFILE_PATH
+
+echo “APP Path - 2”
+
+echo $PROFILE_NAME
 
 
 # Add certificates to keychain and allow codesign to access them
