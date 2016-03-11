173426a93c487a2e66c80f959b3599061d84684a
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2016-03-11 16:31:46 +0530

    Modified config.sh with all the variables

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,2 +1,35 @@
 #!/bin/sh
 export APP_FULLNAME=SwiftAPP
+
+#DEVELOPER_NAME : must have the name appeared on the certificate used. Generally this should not be changed.
+export DEVELOPER_NAME="iPhone Distribution: V2Solutions, Inc."
+#PROFILE_NAME : Certificate profile name. Generally this should not be changed
+export PROFILE_NAME="Distribution"
+#APP_PROFILE_PATH : GITHUB repository relative path for the provisioning profile.
+#                   Standard file already available in respective folder.
+#                   DO not change until scripts or profile folder name changed.
+export APP_PROFILE_PATH="./scripts/profile/"
+#APP_SCRIPT_PATH : Shell script path which is referred internally. Do not change until scripts folder name changed.
+export APP_SCRIPT_PATH="./scripts/certs/"
+#APP_APPLE_CERTIFICATE : Apple standard certificate. Do not changed until required.
+export APP_APPLE_CERTIFICATE="apple.cer"
+#APP_TEAM_AGENT_DISTRIBUTION_CERTIFICATE : Certificated generated from Apple portal.
+#                                          Do not change until certificate is expired / regenerated
+export APP_TEAM_AGENT_DISTRIBUTION_CERTIFICATE="Certificates.cer"
+#APP_TEAM_DISTRIBUTION_PRIVATE_KEYS :  Private key generated using key chain. Do not change until regenerated.
+export APP_TEAM_DISTRIBUTION_PRIVATE_KEYS="Key.p12"
+#APP_NAME : Name of the .app and .ipa file
+export APP_NAME="LendingTree"
+#APP_WORKSPACE_PATH : Relative path of the .xcworkspace file
+- APP_WORKSPACE_PATH="./V2TravisSwifRND.xcworkspace"
+#APP_SCHEME : Name of the xCode scheme. It is assume that all the scheme has the same name.
+#             It is automatically suffixed by scheme (DEV/QA/UAT)
+export APP_SCHEME="Money Center"
+#Settings For Getting Pivotal Notes : Settings For Getting Pivotal Notes
+export INFOPLIST_FILE="./V2TravisSwifRND/Info.plist"
+#- PIVOTAL_PROJECT_ID=1114232
+#- PIVOTAL_TOKEN='763aa9881c2d64966f6c17be60926221'
+export OUTPUT_FILE_NAME='jsonresponse.text'
+#DEV_HOCKEY_APP_ID : HOCKEY APP ID and Token for DEV scheme
+export DEV_HOCKEY_APP_ID="8b8b2bcd857148ddaa5d6428c5212874"
+export DEV_HOCKEY_APP_TOKEN="9a172758bdfe4fe4b10299bfc9ea519a"
