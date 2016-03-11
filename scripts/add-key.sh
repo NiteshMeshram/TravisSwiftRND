1dd9ac9255dcab01e4ca8aefbf3960f645021b3c
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2016-03-11 12:17:53 +0530

    Modified script permissions

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -17,30 +17,29 @@ security unlock-keychain -p travis ios-build.keychain
 
 # Set keychain timeout to 1 hour for long builds
 # see http://www.egeek.me/2013/02/23/jenkins-and-xcode-user-interaction-is-not-allowed/
+echo "Line 1"
+echo $PWD$APP_SCRIPT_PATH$APP_APPLE_CERTIFICATE
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
-echo “APP Path - 1”
-echo $APP_PROFILE_PATH
-
-echo “APP Path - 2”
-
-echo $PROFILE_NAME
 
 
 # Add certificates to keychain and allow codesign to access them
-security import "$APP_SCRIPT_PATH$APP_APPLE_CERTIFICATE" -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import "$APP_SCRIPT_PATH$APP_TEAM_AGENT_DISTRIBUTION_CERTIFICATE" -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import "$APP_SCRIPT_PATH$APP_TEAM_DISTRIBUTION_PRIVATE_KEYS" -k ~/Library/Keychains/ios-build.keychain -P '' -T /usr/bin/codesign
+security import "$PWD$APP_SCRIPT_PATH$APP_APPLE_CERTIFICATE" -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import "$PWD$APP_SCRIPT_PATH$APP_TEAM_AGENT_DISTRIBUTION_CERTIFICATE" -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import "$PWD$APP_SCRIPT_PATH$APP_TEAM_DISTRIBUTION_PRIVATE_KEYS" -k ~/Library/Keychains/ios-build.keychain -P '' -T /usr/bin/codesign
+
+echo "Line 2"
+#security import $PWD/scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+#security import "$APP_SCRIPT_PATH$APP_TEAM_AGENT_DISTRIBUTION_CERTIFICATE" -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+#security import "$APP_SCRIPT_PATH$APP_TEAM_DISTRIBUTION_PRIVATE_KEYS" -k ~/Library/Keychains/ios-build.keychain -P '' -T /usr/bin/codesign
+
+
 #security import ./scripts/certs/Key.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 
 
+
 # Put the provisioning profile in place
-echo “APP Path - 1”
-echo $APP_PROFILE_PATH
-
-echo “APP Path - 2”
-
-echo $PROFILE_NAME
+echo "Line 3"
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp "$APP_PROFILE_PATH$PROFILE_NAME.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
